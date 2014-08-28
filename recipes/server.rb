8c2a8be143ed2c154331d035d11ccdcc8616343f
Daniel Sloof <goapsychadelic@gmail.com>
2014-08-28 14:38:39 +0200

    update new download url

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,8 +2,10 @@ include_recipe 'selenium::default'
 
 directory node['selenium']['server']['installpath']
 
+major, minor, patch = node['selenium']['server']['version'].split('.')
+
 remote_file File.join(node['selenium']['server']['installpath'], 'selenium-server-standalone.jar') do
-  source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+  source "http://selenium-release.storage.googleapis.com/#{major}.#{minor}/selenium-server-standalone-#{major}.#{minor}.#{patch}.jar"
   action :create
   mode 0644
 end
